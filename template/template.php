589f858ee6a11a00967aec785700f41219c6a491
M-Doja <micdoja@yahoo.com>
2018-12-09 02:53:31 -0800

    added meta tags

--- template/template.php
+++ template/template.php
@@ -3,6 +3,7 @@
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="This is the official Center of the World website. Located in Felicity, CA, it is host to a number of attractions such as The Museum of History in Granite and the Church on the Hill.">
+  <meta name="keywords" content="museum, granite, Felicity, center, world, maze, honor">
   <title><?php pageTitle(); ?> | <?php siteName(); ?>&reg;</title>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   <head>
