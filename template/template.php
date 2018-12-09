b464ab38abcbb285b195f875306e633e6cf488e0
M-Doja <micdoja@yahoo.com>
2018-12-09 02:08:13 -0800

    adj ctw page jpg

--- template/template.php
+++ template/template.php
@@ -178,14 +178,14 @@
           <section id="ctw-page" class="pdfImg btn1">
             <a href="#" data-toggle="modal" data-target="#settingCTWModal" class="modal-launcher">
                 <b>Setting The Official <br>Center of The World </b><br>
-              <img id="ctwImg"src="./content/assets/images/2018 Recent book Page 11Center of the World-1.jpg" alt="Image of typed paper about the Center of The World" height="200px">
+              <img id="ctwImg"src="./content/assets/images/2018 Recent book Page 11Center of the World.jpg" alt="Image of typed paper about the Center of The World" height="200px">
             </a>
             <!-- Modal -->
             <div class="modal fade" style="margin-top:75px;" id="settingCTWModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
               <div class="modal-dialog" role="document" style="max-width:732px; width:100%;">
                 <div class="modal-content" >
                   <div class="modal-body">
-                    <img id="" src="./content/assets/images/2018 Recent book Page 11Center of the World-1.jpg"  />
+                    <img id="" src="./content/assets/images/2018 Recent book Page 11Center of the World.jpg"  />
                   </div>
                   <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
