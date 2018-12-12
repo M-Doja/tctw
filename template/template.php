a40649158dc521649a89ee986768ff7b578f2d91
M-Doja <micdoja@yahoo.com>
2018-12-12 15:56:51 -0800

    adj jquery version

--- template/template.php
+++ template/template.php
@@ -373,10 +373,11 @@
     <div class="center footer">
       <footer><small>&copy;<?php echo date('Y'); ?> <?php siteName(); ?>.<br><?php siteVersion(); ?></small></footer>
     </div>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.lazy/1.7.4/jquery.lazy.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
 
     <!-- <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script> -->
+
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.lazy/1.7.4/jquery.lazy.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
@@ -384,21 +385,18 @@
     $(function() {
        $('.lazy').lazy();
    });
-    $(window).load(function() {
+    $(window).on('load', function() {
       if (document.readyState == 'complete') {
-
         $(".loader").fadeOut("slow");
       }
     });
-    $('.carousel').carousel({
-      interval: 2000
-    });
+    $('.carousel').carousel({interval: 2000});
     $('.carousel').on('mouseover', function(){
       $('.carousel').carousel({
         pause: true,
         interval: false
       });
-    })
+    });
     $('body').scrollspy({ target: '#navbar-example2' });
     // When the user scrolls the page, execute myFunction
     window.onscroll = function() {myFunction()};
