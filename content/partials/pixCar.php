3fccf8099079273d24624ddb15cffb544c4f3a40
M-Doja <micdoja@yahoo.com>
2018-12-12 15:23:54 -0800

    adj image overlay

--- content/partials/pixCar.php
+++ content/partials/pixCar.php
@@ -47,8 +47,8 @@ function showCarousalImage($imgName){
     width: 100%;
     height: 100%;
     z-index: 9999;
-    background: url('./content/assets/loading.gif') 50% 50% no-repeat rgb(249,249,249);
-    opacity: .8;
+    background: url('./content/assets/loading.gif') 50% 50% no-repeat;
+    /* opacity: .8; */
 }
 </style>
 <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
